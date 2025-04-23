3d19aadc7a6dfee3860e1fbb9bf11ebff6ed6fe0
naman <namanguptabhopal@gmail.com>
2025-04-23 19:50:47 +0530

    minor update

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.json());
 app.use("/", schoolRoutes);
 
+// Catch-all for unmatched routes (no custom error)
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
--- src/routes/schoolRoutes.js
+++ src/routes/schoolRoutes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const { addSchool, listSchools } = require("../controllers/schoolController");
 
 const router = express.Router();
-
+// Add a simple home route
+router.get("/", (req, res) => {
+    res.json({ message: "Welcome to School API" });
+  });
+  
 router.post("/addSchool", addSchool);
 router.get("/listSchools", listSchools);
 
